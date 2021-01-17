80ecf95965482fe43cca350d848d8cec616485fa
Anessy <n.repekha@gmail.com>
2021-01-17 17:07:11 +0200

    all project

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-main class="v-main">
+      <Header/>
+      <HelloWorld/>
+      <Footer />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Header from './components/Header'
+import Footer from '@/components/Footer'
+
+export default {
+  name: 'App',
+  components: {
+    Footer,
+    Header,
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+  .v-main {
+    min-height: 100vh;
+    font-family: Arial
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Дневник заядлого читателя</h1>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
